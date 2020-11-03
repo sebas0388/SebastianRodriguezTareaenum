21107bb049c7de953abed7485cd9fbe65f0d64d5
sebas0388 <rodriguezjs88@gmail.com>
2020-11-03 09:45:41 -0300

    Se pasan los metodos a la clase

--- src/tareaenum/TareaEnum.java
+++ src/tareaenum/TareaEnum.java
@@ -20,115 +20,52 @@ public class TareaEnum {
      */
     public static void main(String[] args) {
     
-    //Declaro las variables a usar.
-    String name , surname, mail, birthdate, rol;
+  //Declaro las variables a usar.
+    String name = "" , surname = "", mail = "", birthdate = "", rol;
    
-    //Pide los datos del nuevo usuario y los valida.
-   do{
-    name=JOptionPane.showInputDialog(null,"Ingrese su nombre: ","Nombre",JOptionPane.QUESTION_MESSAGE);
-     }while(!validateStr(name));    
-   do{
-    surname=JOptionPane.showInputDialog(null,"Ingrese su apellido: ","Appelido",JOptionPane.QUESTION_MESSAGE);
-      }while(!validateStr(surname));    
-   do{
-    mail=JOptionPane.showInputDialog(null,"Ingrese su mail: ","Mail",JOptionPane.QUESTION_MESSAGE);
-      }while(!validateMail(mail));
-   do{
-    birthdate=JOptionPane.showInputDialog(null,"Ingrese su fecha de nacimiento con el siguiente formato(DD/MM/AAAA): ",
-            "Fecha de nacimiento",JOptionPane.QUESTION_MESSAGE);
-    }while(!validateDate(birthdate));
+    user userEmpty = new user();
+    
+    
+  //Pide los datos del nuevo usuario y los valida.
+    do{
+     name=JOptionPane.showInputDialog(null,"Ingrese su nombre: ","Nombre",JOptionPane.QUESTION_MESSAGE);
+      }while(!userEmpty.validateStr(name));   
+      
+    do{
+     surname=JOptionPane.showInputDialog(null,"Ingrese su apellido: ","Appelido",JOptionPane.QUESTION_MESSAGE);
+       }while(!userEmpty.validateStr(surname));    
    
-   rol = String.valueOf(JOptionPane.showInputDialog(null,"Seleccione el rol del usuario",
+    do{
+     mail=JOptionPane.showInputDialog(null,"Ingrese su mail: ","Mail",JOptionPane.QUESTION_MESSAGE);
+       }while(!userEmpty.validateMail(mail));
+   
+    do{
+     birthdate=JOptionPane.showInputDialog(null,"Ingrese su fecha de nacimiento con el siguiente formato(DD/MM/AAAA): ",
+             "Fecha de nacimiento",JOptionPane.QUESTION_MESSAGE);
+     }while(!userEmpty.validateDate(birthdate));
+   
+    rol = String.valueOf(JOptionPane.showInputDialog(null,"Seleccione el rol del usuario",
                                                         "Roles",JOptionPane.QUESTION_MESSAGE,
                                                         null,roles.values(),0));
-      
-   
-   
-    user userone = new user(name, surname, mail, birthdate);
     
-    //Llamada a la funcion para saber la edad.
-    int age = birthday (birthdate);
+      user userone = new user(name, surname, mail, birthdate);
    
-    //Muestra en pantalla los datos del nuevo usuario.
-    JOptionPane.showMessageDialog(null, "Datos del nuevo usuario: \n" 
+  //Llamada al metodo para saber la edad.
+      userone.birthday(birthdate);
+   
+ //Muestra en pantalla los datos del nuevo usuario.
+      JOptionPane.showMessageDialog(null, "Datos del nuevo usuario: \n" 
             + "\nNombre: " + userone.getName() 
             + "\nApellido: " + userone.getSurname() 
             + "\nMail: " + userone.getMail()
             + "\nFecha de nacimiento: " + userone.getBirthdate()
-            + "\nEdad: " + age
+            + "\nEdad: " + userone.getAge()
             + "\nRol: " + rol
             , "Datos del usuario", JOptionPane.INFORMATION_MESSAGE);
-     
-     }
-    
-    //Los distintos metodos utilizados en el programa para validar los datos
-    public static boolean validateStr(String wordValid){
-    
-    boolean validate= true;
-    
-    if (wordValid.isEmpty()){
-        validate = false;
-      }        
-    return validate;
-         }
-    
-    
- private static boolean validateMail(String wordValid){
-    
-   
-    int num=wordValid.indexOf("@", 0);
-    
-   if (num <0 ){
-        return false;
-      }        
-   if(wordValid.isEmpty()){
-    return false;
-      }
-   return true;
-      }
-    
-private static boolean validateDate(String wordValid){
-    
-   if (wordValid.length()<8){
-       return false;
        }
-   if(wordValid.isEmpty()){
-        return false;
-      }        
-    return true;
-         }
+     }
 
-
-    //Metodo para calcular la edad apartir de la fecha de nacimiento ingresada por el usuario.
- private static int birthday (String birthdate) {
-    
-        DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate today = LocalDate.now(); 
-        String creationDate = today.format(myFormat);
-    
-        
-        String [] Birth = birthdate.split("/");
-        int dayBirth = Integer.parseInt(Birth[0]);
-        int monthBirth = Integer.parseInt(Birth[1]);
-        int yearBirth = Integer.parseInt(Birth[2]);
-        
-        String [] Now = creationDate.split("/");
-        int dayNow = Integer.parseInt(Now[0]);
-        int monthNow = Integer.parseInt(Now[1]);
-        int yearNow = Integer.parseInt(Now[2]);
-                  
-        int years = yearNow - yearBirth;
-        
-        if(monthNow<monthBirth){ 
-             years = years-1;              
-           }
-        if((monthNow==monthBirth) && (dayNow<dayBirth)){
-            years = years-1;            
-           }
-             return years;  
-              
-            }
  
-         }
+         
 
 
--- src/tareaenum/user.java
+++ src/tareaenum/user.java
@@ -5,6 +5,9 @@
  */
 package tareaenum;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author SebastianR
@@ -14,31 +17,40 @@ public class user {
     //Declaro las varables a usar.
     
     private String name, surname, mail, birthdate;  
+    private int age;
     private roles rol;
        
-    //Inicializo constructor.    
+    //Inicializo constructor. 
+    
+    public user() {
+      }
+
     public user(String name, String surname, String mail, String birthdate) {
         this.name = name;
         this.surname = surname;
         this.mail = mail;
         this.birthdate = birthdate;
-      }
+    }
 
     //Seters y Geters
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
-    public void setSurname(String surname) {
+    private void setSurname(String surname) {
         this.surname = surname;
     }
 
-    public void setMail(String mail) {
+    private void setMail(String mail) {
         this.mail = mail;
     }
 
-    public void setBirthdate(String birthdate) {
+    private void setBirthdate(String birthdate) {
         this.birthdate = birthdate;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
     }
 
         
@@ -57,9 +69,83 @@ public class user {
     public String getBirthdate() {
         return birthdate;
     }
+
+    public int getAge() {
+        return age;
+    }
     
-       
+  //Los distintos metodos utilizados en el programa para validar los datos
+    public boolean validateStr(String wordValid){
     
-      }
+    boolean validate= true;
+    
+    if (wordValid.isEmpty()){
+        validate = false;
+      }        
+    return validate;
+         }
+    
+    
+    public boolean validateMail(String wordValid){
+    
+   
+      int num=wordValid.indexOf("@", 0);
+      int num2=wordValid.indexOf(".", 0);
+    
+       if (num <0 ){
+        return false;
+          }    
+       if (num2 <0 ){
+        return false;
+          }        
+       if(wordValid.isEmpty()){
+        return false;
+          }
+        return true;
+          }
+    
+    public boolean validateDate(String wordValid){
+    
+      if (wordValid.length()<8){
+       return false;
+          }
+      if(wordValid.isEmpty()){
+        return false;
+          }        
+        return true;
+          }
+
+
+  //Metodo para calcular la edad apartir de la fecha de nacimiento ingresada por el usuario.
+    public void birthday (String birthdate) {
+    
+        DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate today = LocalDate.now(); 
+        String creationDate = today.format(myFormat);
+    
+        
+        String [] Birth = birthdate.split("/");
+        int dayBirth = Integer.parseInt(Birth[0]);
+        int monthBirth = Integer.parseInt(Birth[1]);
+        int yearBirth = Integer.parseInt(Birth[2]);
+        
+        String [] Now = creationDate.split("/");
+        int dayNow = Integer.parseInt(Now[0]);
+        int monthNow = Integer.parseInt(Now[1]);
+        int yearNow = Integer.parseInt(Now[2]);
+                  
+        int years = yearNow - yearBirth;
+        
+        if(monthNow<monthBirth){ 
+             years = years-1;              
+           }
+        if((monthNow==monthBirth) && (dayNow<dayBirth)){
+            years = years-1;            
+           }
+             setAge(years);  
+              
+            }  
+    
+       }
     
     
